4c8cdbf17ca2b20d093d95dffc5bf17b35735ad1
priyanshi-a-star <73273953+priyanshi-a-star@users.noreply.github.com>
2023-12-03 00:24:57 +0530

    Add files via upload

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFF309B66);
+const Color tdBlue = Color(0xFF06013D);
+const Color tdBlack = Color(0xFF3A3A3A);
+const Color tdGrey = Color(0xFF982D2D);
+const Color tdBGColor = Color(0xFFEEEFF5);
